d18f59c99565d1e3f191f2a6054b12840b62d01e
ericobi <ericobi.lee@gmail.com>
2023-05-17 10:01:59 -0400

    db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,19 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'project_management',
+      entities: ['src/**/**.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
